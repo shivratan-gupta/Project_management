10fa9e4df9dc4197af932716053870c6c36f3e4b
shivratan <shiv58ratan@gmail.com>
2019-01-17 14:47:38 +0000

    added nil check for user task

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,11 +1,11 @@
 module DashboardsHelper
 
 	def completed_task(projectid)
-		user_task_list = UserTask.where(:project_id => projectid,:task_status => 2)
+		user_task_list = UserTask.where(:project_id => projectid,:task_status => 2) if project_id.present? 
 	end
 
 	def in_progress_task(projectid)
-		user_task_list = UserTask.where(:project_id => projectid,:task_status => 1)
+		user_task_list = UserTask.where(:project_id => projectid,:task_status => 1) if project_id.present?
 	end
 
 	def new_task(projectid,task_ids_at_user_level)
@@ -13,14 +13,14 @@ module DashboardsHelper
 	end
 
 	def task_detail(task_id)
-		Task.find(task_id)
+		Task.find(task_id) if task_id.present?
 	end
 
 	def project_detail(project_id)
-		Project.find(project_id)
+		Project.find(project_id) if project_id.present?
 	end
 
 	def user_detail(user_id)
-		User.find(user_id)
+		User.find(user_id) if user_id.present?
 	end
 end
